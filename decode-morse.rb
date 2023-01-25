449f5eb016668937cd6a67a04984f41eb3b935f6
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-01-25 16:42:38 +0200

    fix linters erorrs

--- decode-morse.rb
+++ decode-morse.rb
@@ -32,9 +32,9 @@ def decode_char(char)
 end
 
 def decode_word(word)
-    word.split(" ").map { |char| decode_char(char) }.join
-    end
+  word.split(" ").map { |char| decode_char(char) }.join
+end
 
-    def decode(message)
-        message.split("   ").map { |word| decode_word(word) }.join(" ")
-    end
+def decode(message)
+  message.split("   ").map { |word| decode_word(word) }.join(" ")
+end
